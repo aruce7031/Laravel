6f35846060738f8e0b473fe4924e27eb91c0e205
aru <shinichi.130707@gmail.com>
2024-05-27 11:00:18 +0900

    別ルーティング追加/debugbar追加前の状態保存

---
+++ src/resources/views/test.blade.php
@@ -0,0 +1,23 @@
+<?php
+
+// echo $_GET[your_name];
+// print($_GET);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="test">test</div>
+    <form method="GET" action="./test.php">
+        氏名
+        <input type="text" name="your_name" id="">
+        <input type="submit" name="" id="" value="送信">
+    </form>
+</body>
+</html>
